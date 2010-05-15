79868c8b90fb329c960b314e8e82895ba91391f0
John Turner <groups@norgg.org>
2010-05-16 00:16:35 +0100

    Added lock button to avoid reloads from typing if desired.

--- etherpad/src/static/js/processing-glue.js
+++ etherpad/src/static/js/processing-glue.js
@@ -1,31 +1,36 @@
-var reload_processing = function() {
+var start_reload_timer = function() {
   if (self.timeout) {
-    clearTimeout(self.timeout);
+    clearTimeout(self.timeout); 
   }
-  self.timeout = setTimeout(function() {
-    jswrapper = document.getElementById('jswrapper');
-    padid = jswrapper.getAttribute("padid");
-    var url = '/ep/pad/export/' + padid + '/latest';
-    AJAX = new window.XMLHttpRequest();
-    if (AJAX) {
-      AJAX.open('GET', url + '?t=' + new Date().getTime(), false);
-      AJAX.send(null);
-      code = AJAX.responseText;
-      if (self.processing) {
-        self.processing.exit();
-      }
-      jswrapper.innerHTML = '<canvas id=procjs></canvas>';
-      try {
-        self.processing = Processing(document.getElementById('procjs'), code);
-      }
-      catch (e) {
-        jswrapper.innerHTML = '<div style="color: red;">' + e + '</div>';
-      }
-    }
-    return false;
-  }, 1000);
+
+  if (!document.getElementById('procLock').checked) {
+    self.timeout = setTimeout(reload_processing, 1000);
+  }
 }
 
-setTimeout(function() {padeditor.ace.setNotifyDirty(reload_processing);},
-           2000);
+var reload_processing = function() {
+  jswrapper = document.getElementById('jswrapper');
+  padid = jswrapper.getAttribute("padid");
+  var url = '/ep/pad/export/' + padid + '/latest';
+  AJAX = new window.XMLHttpRequest();
+  if (AJAX) {
+    AJAX.open('GET', url + '?t=' + new Date().getTime(), false);
+    AJAX.send(null);
+    code = AJAX.responseText;
+    if (self.processing) {
+      self.processing.exit();
+    }
+    jswrapper.innerHTML = '<canvas id=procjs></canvas>';
+    try {
+      self.processing = Processing(document.getElementById('procjs'), code);
+    }
+    catch (e) {
+      jswrapper.innerHTML = '<div style="color: red;">' + e + '</div>';
+    }
+  }
+  return false;
+}
+
+setTimeout(function() {padeditor.ace.setNotifyDirty(start_reload_timer);}, 0);
+
 reload_processing();
